03def27a58f371edc0f22abe8acba66a3461e2b0
mihaibucur <a.mihai.bucur@gmail.com>
2017-08-20 00:30:49 +0300

    Added RatingService

---
+++ src/main/java/com/dbtechschool/services/RatingService.java
@@ -0,0 +1,20 @@
+package com.dbtechschool.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dbtechschool.models.Rating;
+import com.dbtechschool.repositories.RatingRepository;
+
+@Service
+public class RatingService {
+	
+	@Autowired
+	RatingRepository ratingRepository;
+	
+	public List<Rating> findAllCourses() {
+		return ((List<Rating>) ratingRepository.findAll());
+	}
+}
