e4b98cf3514d85a7853edb6038fe1b2f92f1d417
Alexandru-Mihail Bucur <a.mihai.bucur@gmail.com>
2017-08-19 12:44:23 +0000

    ResourceRepository.java edited online with Bitbucket

--- src/main/java/com/dbtechschool/repositories/ResourceRepository.java
+++ src/main/java/com/dbtechschool/repositories/ResourceRepository.java
@@ -1,10 +1,10 @@
 package com.dbtechschool.repositories;
 
-import com.dbtechschool.models.User;
+import com.dbtechschool.models.Resource;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DayRepository extends CrudRepository<User, Long> {
+public interface ResourceRepository extends CrudRepository<Resource, Long> {
 
 }
