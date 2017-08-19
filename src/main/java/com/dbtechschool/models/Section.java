42057567f5f259e4984d9361796a2d02435d9885
Bogdan Cristian <rauch.work@yahoo.com>
2017-08-19 15:14:15 +0000

    Section.java edited online with Bitbucket

--- src/main/java/com/dbtechschool/models/Section.java
+++ src/main/java/com/dbtechschool/models/Section.java
@@ -1,21 +1,104 @@
 package com.dbtechschool.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-public class User {
+@Table(name = "SECTIONS")
+public class Section {
 	
 	@Id
+	@Column(name="ID_SECTION")
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+	private int idSection;
 	
-	private String lastName;
-	private String firstName;
-	private String email;
-	private String username;
-	private String password;
-	
+	private String name;
+	private Date start;
+	private Date end;
+	private String room;
+	private String trainer;
+	private String description;
+	private double ratingAvg;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "DAYS")
+	private Day day;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getRatingAvg() {
+		return ratingAvg;
+	}
+
+	public void setRatingAvg(double ratingAvg) {
+		this.ratingAvg = ratingAvg;
+	}
+
+	public Day getDay() {
+		return day;
+	}
+
+	public void setDay(Day day) {
+		this.day = day;
+	}
+
+	@Override
+	public String toString() {
+		return "Section{" +
+				"idSection=" + idSection +
+				", name='" + name + '\'' +
+				", start=" + start +
+				", end=" + end +
+				", room='" + room + '\'' +
+				", trainer='" + trainer + '\'' +
+				", description='" + description + '\'' +
+				", ratingAvg=" + ratingAvg +
+				", day=" + day +
+				'}';
+	}
 }
