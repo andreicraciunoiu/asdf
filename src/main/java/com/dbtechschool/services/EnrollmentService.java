a57d51156bc9773bbb29cb7b8e2236de8a8b273f
mihaibucur <a.mihai.bucur@gmail.com>
2017-08-20 00:25:56 +0300

    Fixed warnings

--- src/main/java/com/dbtechschool/services/EnrollmentService.java
+++ src/main/java/com/dbtechschool/services/EnrollmentService.java
@@ -5,12 +5,8 @@ import com.dbtechschool.repositories.EnrollmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by nicoleta on 8/19/2017.
- */
 
 @Service
 public class EnrollmentService {
