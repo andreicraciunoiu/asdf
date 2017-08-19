081c77b5d449cbf9514a90c7001a3c69ad5856be
mihaibucur <a.mihai.bucur@gmail.com>
2017-08-19 17:56:25 +0300

    course service added

---
+++ src/main/java/com/dbtechschool/services/CourseService.java
@@ -0,0 +1,21 @@
+package com.dbtechschool.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dbtechschool.models.Course;
+import com.dbtechschool.repositories.CourseRepository;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	private CourseRepository courseRepository;
+
+	public List<Course> findAllCourses() {
+		return ((List<Course>) courseRepository.findAll());
+	}
+}
