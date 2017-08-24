0d2a66ecaa2d691bd143d552efe0637881d325e8
Mihai Bucur <a.mihai.bucur@gmail.com>
2017-08-24 19:41:30 +0300

    Fix warning ResourceRepository

--- src/main/java/com/dbtechschool/repositories/ResourceRepository.java
+++ src/main/java/com/dbtechschool/repositories/ResourceRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
-
 @Repository
 public interface ResourceRepository extends CrudRepository<Resource, Integer> {
 
